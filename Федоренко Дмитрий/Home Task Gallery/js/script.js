5fc51ac300222b10bfd56d2e212b0f58572fb5d0
dimoniel <fdn25@bk.ru>
2015-12-21 17:04:19 +0200

    Home Task Gallery has been rebuilded

--- Федоренко Дмитрий/Home Task Gallery/js/script.js
+++ Федоренко Дмитрий/Home Task Gallery/js/script.js
@@ -1,79 +1,141 @@
 // JavaScript Document
-window.onload = init;
+window.onload = function() {
+	var slideBox = document.getElementsByTagName("UL")[0];
+	
+	initGallery(slideBox, true, true);
+	
+	function initGallery(slideContainer, prevNextButtons, pagingButtons) {
+		if (!!slideContainer && (slideContainer.nodeName == "UL")) {
+			window.gallery = new Gallery(slideContainer);
+			gallery.create();
+			
+			if (!!prevNextButtons) {
+				gallery.prevNext();
+			}
+			
+			if (!!pagingButtons) {
+				gallery.pagination();
+			}
+		
+		} else {
+			alert("Список не найден");
+		}
+			
+	};
 
-function init() {
-	var gallery = document.getElementsByClassName("gallery")[0];
-	var slideContainer = gallery.getElementsByTagName("UL")[0];
-	var galleryElements = gallery.getElementsByTagName("LI");
+};
+
+function Gallery(containerUL) {
+	var wrapper = document.getElementsByTagName("DIV")[0];
+	var holdGallery = document.createElement("DIV");
 	var galleryElementsArray = [];
 	var count = 0;
 	var leftMargin = 0;
 	
-	for (var i = 0; i < galleryElements.length; i++) {
-		galleryElementsArray.push(galleryElements[i]);
-	}
-	
-	control();
-	
-	function control() {
-		var paggingLi = document.getElementsByClassName("paging")[0].getElementsByTagName("LI");
-		var paggingLinksArray = [];
-		document.getElementsByClassName("prev")[0].getElementsByTagName("a")[0].addEventListener("click", previous, false);
-		document.getElementsByClassName("next")[0].getElementsByTagName("a")[0].addEventListener("click", next, false);
+	Gallery.prototype.create = function() {
+		var holdList = document.createElement("DIV");
 		
-		for (var i = 0; i < paggingLi.length; i++) {
-			paggingLinksArray[i] = paggingLi[i].getElementsByTagName("a")[0];
-			paggingLinksArray[i].addEventListener("click", pagging, false);
+		holdGallery.setAttribute("class", "gallery-holder");
+		holdList.setAttribute("class", "gallery");
+		holdList.appendChild(containerUL);
+		holdGallery.appendChild(holdList);
+		wrapper.appendChild(holdGallery);
+		
+		var galleryElements = containerUL.getElementsByTagName("LI");
+		for (var i = 0; i < galleryElements.length; i++) {
+			galleryElementsArray.push(galleryElements[i]);
 		}
+	};
+	
+	this.prevNext = function() {
+		var prevButton = document.createElement("DIV");
+		var nextButton = document.createElement("DIV");
+		
+		holdGallery.style.width = holdGallery.offsetWidth + 120 + "px";
+		
+		prevButton.setAttribute("class", "prev");
+		prevButton.innerHTML = '<a href="#" title="Previous">Previous</a>';
+		holdGallery.appendChild(prevButton);
+		
+		nextButton.setAttribute("class", "next");
+		nextButton.innerHTML = '<a href="#" title="Next">Next</a>';
+		holdGallery.appendChild(nextButton);
+		
+		prevButton.addEventListener("click", previous, false);
+		nextButton.addEventListener("click", next, false);
 		
 		function previous() {
 			if (count == 0) {
-				for (var i = 0; i < galleryElements.length - 1; i++) {
+				for (var i = 0; i < galleryElementsArray.length - 1; i++) {
 					leftMargin += galleryElementsArray[i].offsetWidth;
 					++count;
 				}
-				slideContainer.style.marginLeft = "-" + leftMargin + "px";
+				containerUL.style.marginLeft = "-" + leftMargin + "px";
 			} else {
 				leftMargin -= galleryElementsArray[count].offsetWidth;
-				slideContainer.style.marginLeft = "-" + leftMargin + "px";
+				containerUL.style.marginLeft = "-" + leftMargin + "px";
 				--count;
 			}
-			changeAttrib(count);
 		};
 		
 		function next() {
 			if (count < galleryElementsArray.length - 1) {
 				leftMargin += galleryElementsArray[count].offsetWidth;
-				slideContainer.style.marginLeft = "-" + leftMargin + "px";
+				containerUL.style.marginLeft = "-" + leftMargin + "px";
 				++count;
 			} else {
 				leftMargin = 0;
-				slideContainer.style.marginLeft = leftMargin;
+				containerUL.style.marginLeft = leftMargin;
 				count = 0;
 			}
-			changeAttrib(count);
 		};
+	};
+	
+	this.pagination = function() {
+		var pagingContainer = document.createElement("DIV");
+		var pagingList = document.createElement("UL");
+		var pagingLinksArray = [];
 		
-		function pagging(e) {
-			var index = paggingLinksArray.indexOf(e.target);
+		pagingContainer.setAttribute("class", "paging");
+		pagingContainer.appendChild(pagingList);
+		holdGallery.appendChild(pagingContainer);
+		
+		for (var i = 0; i < galleryElementsArray.length; i++) {
+			pagingLinksArray[i] = document.createElement("LI");
+			pagingLinksArray[i].innerHTML += '<a href="#" title="' + (i+1) + '">' + (i+1) + '</a>';
+			pagingList.appendChild(pagingLinksArray[i]);
+			pagingLinksArray[i].addEventListener("click", paging, false);
+		}
+		
+		pagingList.firstElementChild.setAttribute("class", "active");
+		
+		
+		function paging(e) {
+			count = (pagingLinksArray.indexOf(e.target.parentNode) > -1) ?
+					pagingLinksArray.indexOf(e.target.parentNode) : count;
 			leftMargin = 0;
-			count = index;
+			
 			for (var i = 0; i < count; i++) {
 				leftMargin += galleryElementsArray[i].offsetWidth;
 			}
 			if (leftMargin == 0) {
-				slideContainer.style.marginLeft = leftMargin;
+				containerUL.style.marginLeft = leftMargin;
 			} else {
-				slideContainer.style.marginLeft = "-" + leftMargin + "px";
+				containerUL.style.marginLeft = "-" + leftMargin + "px";
 			}
-			changeAttrib(count);
-		}
-		
-		function changeAttrib(number) {
-			for (var i = 0; i < paggingLi.length; i++) {
-				paggingLi[i].removeAttribute("class");
-			}
-			paggingLi[number].setAttribute("class", "active");
 		};
+		
+		holdGallery.addEventListener("click", changeAttrib, false);
+		
+		function changeAttrib() {
+			for (var i = 0; i < pagingLinksArray.length; i++) {
+				pagingLinksArray[i].removeAttribute("class");
+			}
+			pagingLinksArray[count].setAttribute("class", "active");
+		};
+	};
+	
+	this.remove = function() {
+		wrapper.removeChild(holdGallery);
 	};
 };
